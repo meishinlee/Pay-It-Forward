0be4f8f3b78063a916042ac91236ec9a6c133a2d
J0NL1N <jl8918@nyu.edu>
2019-09-28 23:09:42 -0400

    testing

---
+++ testing.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/home')
+def home(): 
+	return render_template("home.html")
+
+@app.route('/donate')
+def donate(): 
+	return render_template("donate.html")
+
+@app.route('/map')
+def map(): 
+	return render_template("map.html")
+	
